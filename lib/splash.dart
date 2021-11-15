7346b41db2f2001fad1a196b188649c809e45e20
60billion <60billions@gmail.com>
2021-11-15 19:27:20 +0900

    fitst init

---
+++ lib/splash.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Splash extends StatelessWidget {
+  const Splash({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.amber,
+      child: const Center(
+          child: CircularProgressIndicator(
+        color: Colors.white,
+      )),
+    );
+  }
+}
