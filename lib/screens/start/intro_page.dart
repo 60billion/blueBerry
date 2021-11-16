a7a46b6aa3f4edc6459015bca92139899fc3ea7e
60billion <60billions@gmail.com>
2021-11-16 16:24:55 +0900

    set custom font

--- lib/screens/start/intro_page.dart
+++ lib/screens/start/intro_page.dart
@@ -9,23 +9,45 @@ class IntroPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Text("블루베리 마켓"),
-        ExtendedImage.asset("assets/imgs/carrot_intro.png"),
-        Text('우리 동네 중고 직거래 블루베리마켓'),
-        Text("블루베리 마켓은 직거래 마켓이에요.\n내 동네를 성정하고 시작해보세요."),
-        TextButton(
-          onPressed: () {
-            logger.d("on text button clicked!");
-          },
-          child: Text(
-            '내 동네 설정하고 시작하기',
-          ),
-          style: TextButton.styleFrom(
-              primary: Colors.white, backgroundColor: Colors.blue),
-        )
-      ],
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              "블루베리 마켓",
+              style: TextStyle(
+                  fontSize: 30.0,
+                  color: Colors.purple,
+                  fontWeight: FontWeight.bold),
+            ),
+            ExtendedImage.asset("assets/imgs/carrot_intro.png"),
+            Text('우리 동네 중고 직거래 블루베리마켓',
+                style: TextStyle(fontSize: 22.0, fontWeight: FontWeight.bold)),
+            Text("블루베리 마켓은 직거래 마켓이에요.\n내 동네를 성정하고 시작해보세요.",
+                style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold)),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                TextButton(
+                  onPressed: () {
+                    logger.d("on text button clicked!");
+                  },
+                  child: Text(
+                    '내 동네 설정하고 시작하기',
+                    style:
+                        TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+                  ),
+                  style: TextButton.styleFrom(
+                      primary: Colors.white, backgroundColor: Colors.purple),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
     );
   }
 }
